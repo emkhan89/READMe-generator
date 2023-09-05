7fea6f5ce1f89e5968790a97d33614758e39ab29
unknown <emkhan89>
2023-09-05 23:29:55 +0100

    added list of questions which user is prompted to answer

--- index.js
+++ index.js
@@ -1,33 +1,47 @@
 // Use ES6 syntax to require the fs file and add the node files
-// Type node index.js in the intergrated terminal to test it.
+// ============================================================
 const fs = require("fs");
 const inquirer = require("inquirer");
 
+// Type node index.js in the intergrated terminal to test it.
+// ==========================================================
+
 // install node files by typing npm init -y  AND   npm i inquirer in the integrated terminal
+// =========================================================================================
+
+// add inquirer prompt, with a list of question which will be written to the readme file.
+// =====================================================================================
+
 inquirer.prompt([
   {
-    type: "input",
-    message: "What is your name?",
-    name: "name",
+    type: "list",
+    message: "What is the title of your project?",
+    name: "filename",
   },
   {
     type: "input",
-    message: "your location",
-    name: "location",
+    message: "Provide a brief description of your project.",
+    name: "description",
   },
   {
     type: "input",
-    message: "your bio",
-    name: "bio",
+    message: "What are the steps required to install your project?",
+    name: "installation",
   },
   {
     type: "input",
-    message: "Your Linkin profile",
-    name: "Linkdin",
+    message: "Provide instructions and examples for use.",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message:
+      "List your collaborators, if any, with links to their Github profiles.",
+    name: "contributors",
   },
   {
     type: "input",
     message: "your Github profile",
-    name: "Github",
+    name: "tests",
   },
 ]);
