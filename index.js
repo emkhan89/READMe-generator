5c319ffb873f3df53bee676f4bf0d947efcb4cc5
unknown <emkhan89>
2023-09-05 22:28:12 +0100

    added node files, inquirer

---
+++ index.js
@@ -0,0 +1,6 @@
+// Use ES6 syntax to require the fs file and add the node files
+// Type node index.js in the intergrated terminal to test it.
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+// install node files by typing npm init -y  AND   npm i inquirer in the integrated terminal
