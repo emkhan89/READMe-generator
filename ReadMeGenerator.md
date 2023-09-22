2c0bbbbcd5bf10ec37ffd268903045b7c0b2494e
unknown <emkhan89>
2023-09-22 21:26:56 +0100

    debugged inquirer install version error and completed the code

---
+++ ReadMeGenerator.md
@@ -0,0 +1,32 @@
+# Read Me Generator
+
+## Table of Contents:
+1. [Description](#description)
+2. [Installation](#installation)
+3. [Usage](#usage)
+4. [Contributors](#contributors)
+5. [License](#license)
+6. [Github](#github)
+7. [Email](#email)
+
+## Description
+A project designed to put my new found skills in Node to the test
+
+## Installation
+Run npm install in the integrated terminal to install the dependencies
+
+## Usage 
+Type node index.js in to the integrated terminal
+
+## Contributors
+None
+
+## License
+MIT License
+
+## Github
+emkhan89
+
+## Email
+undefined
+
--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 // ============================================================
 const fs = require("fs");
 const inquirer = require("inquirer");
+const util = require("util");
 // import fs from "fs";
 // import inquirer from "inquirer";
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // Type node index.js in the intergrated terminal to test it.
 // ==========================================================
@@ -14,46 +16,103 @@ const inquirer = require("inquirer");
 // add inquirer prompt, with a list of question which will be written to the readme file.
 // =====================================================================================
 
-inquirer.prompt([
-  {
-    type: "input",
-    message: "What is the title of your project?",
-    name: "filename",
-  },
-  {
-    type: "input",
-    message: "Provide a brief description of your project.",
-    name: "description",
-  },
-  {
-    type: "input",
-    message: "What are the steps required to install your project?",
-    name: "installation",
-  },
-  {
-    type: "input",
-    message: "Provide instructions and examples for use.",
-    name: "usage",
-  },
-  {
-    type: "input",
-    message:
-      "List your collaborators, if any, with links to their Github profiles.",
-    name: "contributors",
-  },
-  {
-    type: "input",
-    message: "your Github profile",
-    name: "tests",
-  },
-  {
-    type: "input",
-    message: "does this test work?",
-    name: "test",
-  },
-]);
+const questions = () =>
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "What is the title of your project?",
+      name: "filename",
+    },
+    {
+      type: "input",
+      message: "Provide a brief description of your project.",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "What are the steps required to install your project?",
+      name: "installation",
+    },
+    {
+      type: "input",
+      message: "Provide instructions and examples for use.",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message:
+        "List your collaborators, if any, with links to their Github profiles.",
+      name: "contributors",
+    },
+    {
+      type: "list",
+      message: "What licenses are required with this project?",
+      name: "license",
+      choices: [
+        "Apache license 2.0",
+        "GNU General Public License v3.0",
+        "MIT License",
+        'BSD 2-clause "Simplified" license',
+        'BSD 3-clause "New" or "Revised" license',
+        "Boost Software License 1.0",
+        "Eclipse Public License 2.0",
+        "GNU Affero General Public License v3.0",
+        "GNU General Public License v2.0",
+        "GNU Lesser General Public License v2.1",
+        "Mozilla Public License 2.0",
+        "The Unlicense",
+      ],
+    },
+    {
+      type: "input",
+      message: "what is your Github username",
+      name: "github",
+    },
+    {
+      type: "input",
+      message: "what is your email address?",
+      name: "emails",
+    },
+  ]);
 
-// TO DO
-// couldnt figure out how to debug the error code
-// write the file to the read me
-// create the function
+const createReadMe = (answers) =>
+  `# ${answers.filename}
+
+## Table of Contents:
+1. [Description](#description)
+2. [Installation](#installation)
+3. [Usage](#usage)
+4. [Contributors](#contributors)
+5. [License](#license)
+6. [Github](#github)
+7. [Email](#email)
+
+## Description
+${answers.description}
+
+## Installation
+${answers.installation}
+
+## Usage 
+${answers.usage}
+
+## Contributors
+${answers.contributors}
+
+## License
+${answers.license}
+
+## Github
+${answers.github}
+
+## Email
+${answers.email}
+
+`;
+
+questions()
+  .then((answers) =>
+    writeFileAsync("ReadMeGenerator.md", createReadMe(answers))
+  )
+  .then(() => console.log("Successfully created ReadMe file"))
+  .catch((err) => console.error(err));
